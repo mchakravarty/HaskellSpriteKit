51daf5624fe75e418fc69a71d6e568fc034f36d5
Manuel M T Chakravarty <chak@justtesting.org>
2014-12-05 16:38:02 +1100

    Dependency wibbles

--- HaskellSpriteKit/HaskellSpriteKit.h
+++ HaskellSpriteKit/HaskellSpriteKit.h
@@ -15,8 +15,3 @@ FOUNDATION_EXPORT double HaskellSpriteKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char HaskellSpriteKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <HaskellSpriteKit/PublicHeader.h>
-
-#import "Color_objc.h"
-#import "Geometry_objc.h"
-#import "Node_objc.h"
-#import "Texture_objc.h"
